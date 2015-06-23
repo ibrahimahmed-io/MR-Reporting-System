fe20a97123510760855ee9e225f55fa270a83f4d
IbrahimAmin <ibrahimtorbany.dev@outlook.com>
2015-06-23 21:24:24 +0200

    Initialization_Day01

---
+++ MR Reporting System/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MR_Reporting_System.Startup))]
+
+namespace MR_Reporting_System
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
